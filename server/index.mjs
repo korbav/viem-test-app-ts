bde24f366527c4e7e49a9e30ef40b17322ad3d71
Bilel <docka2000-2@yahoo.fr>
2023-09-13 22:57:09 +0200

    preparing for serving from VM

---
+++ server/index.mjs
@@ -0,0 +1,21 @@
+import path from "path";
+import express from "express";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express(); // create express app
+
+// add middlewares
+app.use(express.static(path.join(__dirname, "..",  "..", "dist")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..",  "..", "dist", "index.html"));
+});
+
+// start express server on port 80
+app.listen(80, () => {
+  console.log("server started on port 80");
+});
