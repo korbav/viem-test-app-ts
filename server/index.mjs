84f44daf2d0feeda61cd4102887158e9debadaf4
Bilel <docka2000-2@yahoo.fr>
2023-09-14 09:46:39 +0200

    fix: server html path

--- server/index.mjs
+++ server/index.mjs
@@ -8,11 +8,11 @@ const __dirname = path.dirname(__filename);
 const app = express(); // create express app
 
 // add middlewares
-app.use(express.static(path.join(__dirname, "..",  "..", "dist")));
+app.use(express.static(path.join(__dirname, "..", "dist")));
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..",  "..", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "..",  "dist", "index.html"));
 });
 
 // start express server on port 80
