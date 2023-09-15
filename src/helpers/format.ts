8125b626210a43f2b4b10f9b39f4494f92ad35e3
Bilel <docka2000-2@yahoo.fr>
2023-09-15 14:02:01 +0200

    fix: bad formatting for values near to 0

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -1,19 +1,7 @@
-export const formatValue = (value: string, decimals = 18) => (!value || value === "0") ? "0" : `${(Number(value.slice(0, value.length - decimals))).toLocaleString("en-US")}.${value.slice(-1 * decimals)}`
-
-/*
-export const formatValueOld = (value: string, decimals = 18) => {
-    if (!value || value === "0") {
+export const formatValue = (value: string, decimals = 18) => {
+    if(!value || value === "0") 
         return "0";
-    }
-    let intPart = value.slice(0, value.length - decimals);
-    let groupSize = 3;
-    let groupsCount = Math.ceil(intPart.length / groupSize);
-    let groups = [];
-    for (let i = 0; i < groupsCount; i++) {
-        if (i == 0) groups.push(intPart.slice(- 1 * groupSize))
-        else
-            groups.push(intPart.slice(-(i + 1) * groupSize, -i * groupSize))
-    }
-    return `${groups.reverse().join(",")}.${value.slice(-1 * decimals)}`
+    else if(value.length < decimals) 
+        return `0.${value}`;
+    return `${(Number(value.slice(0, value.length - decimals))).toLocaleString("en-US")}.${value.slice(-1 * decimals)}`
 }
-*/
