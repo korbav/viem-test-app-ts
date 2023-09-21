df4835c606012cbc32a473bcca0547965793f4ca
Bilel <docka2000-2@yahoo.fr>
2023-09-21 17:04:14 +0200

    adding address link to polygon scan  on allowances data

--- src/components/Operations/Allowance/Allowance.tsx
+++ src/components/Operations/Allowance/Allowance.tsx
@@ -8,7 +8,11 @@ export default function Allowance({ action }: { action: any }) {
         <Stack direction={"column"} gap={0}>
             <Stack direction={"row"} gap={1}>
                 <Typography className="w-14 min-w-14 max-w-14 text-left shrink-0" fontSize={fontSize}>User</Typography>
-                <Typography fontSize={fontSize} className="text-blue-700 overflow-hidden overflow-ellipsis">{action.user}</Typography>
+                <Typography fontSize={fontSize} className="text-blue-700 overflow-hidden overflow-ellipsis">
+                    <a target="_blank" className="text-neutral-500 underline decoration-dotted" title={action.user.toString()} href={`https://mumbai.polygonscan.com/address/${action.user.toString()}`}>
+                        {action.user}
+                    </a>
+                </Typography>
             </Stack>
             <Stack direction={"row"} gap={1}>
                 <Typography className="w-14 min-w-14 max-w-14 text-left shrink-0" fontSize={fontSize}>Value</Typography>
