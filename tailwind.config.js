7d6d46ca800b71c153c755fb30e1a1e839b1ff1a
Bilel <docka2000-2@yahoo.fr>
2023-09-25 09:57:47 +0200

    better higlighting of new transactions

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ export default {
     extend: {
       keyframes: {
         brightness: {
-          '0%': {   background:  'rgba(255,255,255,1)' },
+          '0%':  {   filter: "blur(1.5px) invert(0.3)", "mask-image": 'linear-gradient(to right, rgba(0, 0, 0, 1),  rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1))', "mask-size": "0% 100%", background:  'rgba(255,255,255,1)' },
           '10%': {  background:  'rgba(210,240,255,1)' },
           '20%': {  background:  'rgba(255,255,255,1)' },
           '30%': {  background:  'rgba(210,240,255,1)' },
-          '60%, 100%': { background:  'rgba(255,255,255,1)' },
+          '60%, 100%': { filter: "blur(0px) invert(0)", "mask-image": 'linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 1))', "mask-size": "100% 100%", background:  'rgba(255,255,255,1)' },
         },
       },
       animation: {
